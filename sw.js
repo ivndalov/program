fc6aa384e8fe70b6aa64ce3fb9da01d499c4fea2
Nazarivskyi Illia <illia.nazarivskyi.crowner@gmail.com>
2026-02-14 01:55:31 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'ourspace-v1';
+const ASSETS = [
+    './',
+'./index.html',
+'./manifest.json',
+'https://cdn.tailwindcss.com',
+'https://cdn.jsdelivr.net/npm/canvas-confetti@1.6.0/dist/confetti.browser.min.js',
+'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
